06ba667c87e1312038cdc6bd68c6ba44fbdabca0
Helene Herin <hlne.herin@gmail.com>
2021-04-02 18:21:17 +0200

    dispatch server + try loop on recv

--- server.cpp
+++ server.cpp
@@ -6,7 +6,7 @@
 /*   By: hherin <hherin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 13:53:22 by hherin            #+#    #+#             */
-/*   Updated: 2021/04/01 14:56:55 by hherin           ###   ########.fr       */
+/*   Updated: 2021/04/02 18:02:20 by hherin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,104 @@ struct timeval*	setTimeval()
 	return &tv;
 }
 
+// try to loop in recv
+std::string const &Server::recv_timeout(int &fd)
+{
+	char buffer[1000];
+	int recvVal, totLen = 0;
+	static std::string req;
+	struct timeval begin, now;
+	double timediff;
+	
+	req.clear();
+	gettimeofday(&begin, NULL);
+	
+	while (1){
+		gettimeofday(&now, NULL);
+		timediff = (now.tv_sec - begin.tv_sec) + 1e-6 * (now.tv_usec - begin.tv_usec);
+
+		if (totLen > 0 && timediff > TIMEOUT)
+			break;
+		else if (timediff > TIMEOUT * 2)	//if no data at all, wait a little longer twice timeout
+			break;
+		printf("1\n");
+		bzero(buffer,256);
+		if ((recvVal = recv(fd, buffer, 1000, 0)) < 0){
+			perror("ERROR recv");
+			exit(1);
+		}
+		else{
+			printf("HEY\n");
+			req.append(buffer);
+			totLen += recvVal;
+			gettimeofday(&begin, NULL);
+		}
+		printf("2\n");
+	}
+
+	return req;
+}
+
+/* Check if the fd with event is a non-server fd */
+void	Server::request_handler()
+{
+	for (int i = 1; i < MAX_CONNECTIONS; i++){
+		if (all_connections[i] > 0 && FD_ISSET(all_connections[i], &_readfd)){
+			printf("Returned fd is %d [index, i: %d]\n", all_connections[i], i);
+			bzero(buffer,256);
+			int n = recv(all_connections[i], buffer, 255, 0);
+			if (n == 0){
+					close(all_connections[i]);
+				all_connections[i] = -1;
+			}
+			else if (n < 0) {
+					perror("ERROR recv");
+				exit(1);
+			}
+			else
+				printf("Here is the message:\n %s\n\n",buffer); // call function that process the request
+
+			// send msg to the client
+			n = send(all_connections[i],"I got your message\n",19, 0);
+			if (n < 0) {
+				perror("ERROR writing to socket");
+				exit(1);
+			}
+						
+			if (!strncmp(bufRecv.c_str(), "close", 5))
+			{
+				close(all_connections[i]);
+			}
+			selectval--;
+			if (!selectval) continue;
+		}	
+	}
+}
+
+int	Server::add_client()
+{
+	if (FD_ISSET(_sockfd, &_readfd)){
+		clilen = sizeof(cli_addr);
+		bzero((char *) &cli_addr, sizeof(cli_addr));
+		if ((_newsockfd = accept(_sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen)) < 0){
+			perror("ERROR on accept");
+			exit(1);
+		}
+		else{
+			printf("Server-accept() is OK...\n");
+			for (int i = 0; i < MAX_CONNECTIONS; i++){
+				if (all_connections[i] < 0){
+					all_connections[i] = _newsockfd;
+					break;
+				}
+			}
+			selectval--;
+			if (!selectval) return 1;
+		}
+	}
+	return 0;
+}
+
 void	Server::cliConnect()
 {
 	for (int i = 0; i < MAX_CONNECTIONS; i++)
@@ -62,79 +160,26 @@ void	Server::cliConnect()
 
 	while (true){
 		FD_ZERO(&_readfd);
-		FD_ZERO(&_writefd);
-
-		for (int i = 0; i < MAX_CONNECTIONS; i++)
-			if (all_connections[i] >= 0){
-				FD_SET(all_connections[i], &_readfd);
-				FD_SET(all_connections[i], &_writefd);
-			}
 		
-		int selectval;
-		// if ((selectval = select(FD_SETSIZE, &_readfd, &_writefd, NULL, setTimeval())) < 0){
-		if ((selectval = select(FD_SETSIZE, &_readfd, NULL, NULL, setTimeval())) < 0){
+		for (int i = 0; i < MAX_CONNECTIONS; i++)
+			if (all_connections[i] >= 0)
+				FD_SET(all_connections[i], &_readfd);
+		
+		// if ((selectval = select(FD_SETSIZE, &_readfd, NULL, NULL, setTimeval())) < 0){
+		if ((selectval = select(FD_SETSIZE, &_readfd, NULL, NULL, NULL)) < 0){				// change SETSIZE for more efficiency
 			perror("ERROR on select");
 			exit(1);
 		}
-		printf("RETOUR SELECT %d\n", selectval);
-		if (selectval == 0){
+		else if (selectval == 0){
 			printf("TIMEOUT \n");
 			exit(1);
 		}
 		else if (selectval >= 0){		 /* select() woke up. Identify the fd that has events */
 			printf("Server-select() is OK...\n");
-
-			if (FD_ISSET(_sockfd, &_readfd)){
-				clilen = sizeof(cli_addr);
-				bzero((char *) &cli_addr, sizeof(cli_addr));
-				if ((_newsockfd = accept(_sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen)) < 0){
-					perror("ERROR on accept");
-					exit(1);
-				}
-				else{
-					printf("Server-accept() is OK...\n");
-					for (int i = 0; i < MAX_CONNECTIONS; i++){
-						if (all_connections[i] < 0){
-							all_connections[i] = _newsockfd;
-							break;
-						}
-					}
-				selectval--;
-				if (!selectval) continue;
-				}
-			}
-
-			/* Check if the fd with event is a non-server fd */
-			for (int i = 1; i < MAX_CONNECTIONS; i++){
-				if (all_connections[i] > 0 && FD_ISSET(all_connections[i], &_readfd)){
-					printf("Returned fd is %d [index, i: %d]\n", all_connections[i], i);
-					bzero(buffer,256);
-					int n = recv(all_connections[i], buffer, 255, 0);
-					if (n == 0){
-						close(all_connections[i]);
-						all_connections[i] = -1;
-					}
-					else if (n < 0) {
-						perror("ERROR recv");
-						exit(1);
-					}
-					else
-						printf("Here is the message: %s\n\n",buffer); // call function that process the request
-						
-					// if (FD_ISSET(all_connections[i], &_writefd)){
-						n = send(all_connections[i],"I got your message\n",19, 0);	// send the result
-						if (n < 0) {
-							perror("ERROR writing to socket");
-							exit(1);
-						}
-						
-					// }
-					
-				}
-				selectval--;
-				if (!selectval) continue;
-			}
-
+			if (add_client())
+				continue;				//go back to beginning of the loop - don't go to request_handler
 		}
+
+		request_handler();				// call only when it's not a connection with a client
 	}
 }
