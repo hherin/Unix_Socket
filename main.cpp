b2023353c8fd0eb32d9eaa9a53250ea48ec24c88
Helene Herin <hlne.herin@gmail.com>
2021-04-26 11:20:25 +0200

    add multiple port to HTTPserver

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: hherin <hherin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 17:07:48 by hherin            #+#    #+#             */
-/*   Updated: 2021/04/26 11:11:37 by hherin           ###   ########.fr       */
+/*   Updated: 2021/04/26 11:19:45 by hherin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,13 @@ int main(int ac, char **av)
     std::vector<int> sockArray;
     for (std::map<int, std::vector<ServerInfo> >::iterator it = m_srv.begin(); it != m_srv.end(); it++)
         sockArray.push_back(it->first);
-    
-    std::cout << "NB socket " << sockArray.size() << "\n";
-	// int sockArray[] = {3490};
 
 	HttpServer server;
 
 	// Creating sockets and binding them to a port
 	try
 	{
-		for (int i = 0; i < 2; ++i)
+		for (size_t i = 0; i < sockArray.size(); ++i)
 			server.addServerSocket(ServerSocket(sockArray[i], 5));
 			
 		server.etablishConnection();
