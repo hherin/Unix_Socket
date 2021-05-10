a20983ab096aea6ea112433c7c2d3ff1018707a4
Helene Herin <hlne.herin@gmail.com>
2021-05-10 15:06:47 +0200

    changes before merging llefranc branch

--- script_test/start.sh
+++ script_test/start.sh
@@ -3,15 +3,15 @@
 #                                                         :::      ::::::::    #
 #    start.sh                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: heleneherin <heleneherin@student.42.fr>    +#+  +:+       +#+         #
+#    By: hherin <hherin@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/28 14:58:40 by heleneherin       #+#    #+#              #
-#    Updated: 2021/05/04 14:06:16 by heleneherin      ###   ########.fr        #
+#    Updated: 2021/05/10 13:47:54 by hherin           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #! /bin/bash 
 
-../webserv ../parser/test/conf > OUTPUT-WEB 2>&1 &
+# ../webserv ../parser/test/conf > OUTPUT-WEB 2>&1 &
 ls -d -1 "$PWD"/req/* > ls_files
 expect -df ./telnet.expect localhost 8080 > OUTPUT-EXP 2>&1
