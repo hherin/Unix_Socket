4c5d2ac8374c19d369acf407913b94a0d1afab50
lucaslefrancq <hlne.herin@gmail.com>
2021-05-10 12:19:32 +0200

    add log file to expect

--- script_test/start.sh
+++ script_test/start.sh
@@ -6,12 +6,12 @@
 #    By: heleneherin <heleneherin@student.42.fr>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/28 14:58:40 by heleneherin       #+#    #+#              #
-#    Updated: 2021/05/03 19:17:13 by heleneherin      ###   ########.fr        #
+#    Updated: 2021/05/04 14:06:16 by heleneherin      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #! /bin/bash 
 
-screen -S webserv -d -m -X ../webserv parser/test/conf > OUTPUT-WEB
+../webserv ../parser/test/conf > OUTPUT-WEB 2>&1 &
 ls -d -1 "$PWD"/req/* > ls_files
-expect -df ./telnet.expect localhost 8080 > OUTPUT-EXP
+expect -df ./telnet.expect localhost 8080 > OUTPUT-EXP 2>&1
