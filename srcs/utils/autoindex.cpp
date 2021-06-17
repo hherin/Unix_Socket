86fce81c7435fad1ba3ef6d965ceefb380d77602
lucaslefrancq <hlne.herin@gmail.com>
2021-06-17 14:03:55 +0200

    autoindex done

--- srcs/utils/autoindex.cpp
+++ srcs/utils/autoindex.cpp
@@ -3,57 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   autoindex.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hherin <hherin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: heleneherin <heleneherin@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 17:42:35 by hherin            #+#    #+#             */
-/*   Updated: 2021/06/16 19:08:43 by hherin           ###   ########.fr       */
+/*   Updated: 2021/06/17 14:02:49 by heleneherin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 #include "../includes/webserv.hpp"
-
-#include <unistd.h>
-
-// std::string const &autoIndexDisplayer(std::string uri)
-// {
-// 	int pid;
-// 	int fd[2];
-// 	static std::string displayList;
-// 	displayList.clear();
-
-
-// 	pipe(fd);
-// 	pid = fork();
-	
-// 	if (!pid){
-// 		dup2(fd[1], STDOUT_FILENO);
-// 		close(fd[0]);
-// 		close(fd[1]);
-// 		std::string cmd = (std::string("php ") + getcwd(NULL, 0)) + "/autoindex.php " + uri;
-// 		std::system(cmd.c_str());
-// 		return displayList;
-// 	}
-// 	else if (pid > 0){
-// 		close(fd[1]);
-	
-// 		char buf[BUFFER_SIZE_CGI_PIPE + 1] = {0};
-// 		while (read(fd[0], buf, BUFFER_SIZE_CGI_PIPE) > 0)
-// 		{
-// 			displayList += buf;
-// 			memset(buf, 0, BUFFER_SIZE_CGI_PIPE + 1);
-// 		}
-// 		displayList += buf;
-
-// 		close(fd[0]);	
-// 	}
-// 	else {
-// 		close(fd[1]); close(fd[0]);
-// 		throw std::runtime_error("Error in fork occurs\n");
-		
-// 	}
-// 	return displayList;
-// }
 
 void autoIndexDisplayer(std::string const &uri, std::string &displayList)
 {
@@ -65,24 +23,14 @@ void autoIndexDisplayer(std::string const &uri, std::string &displayList)
 	close(fd[1]);
 	
 	std::string cmd = "php " + (getcwd(NULL, 0) + ("/autoindex.php " + uri)) ;
-	std::cerr << "avant std::system\n";
-	std::cout << std::system(cmd.c_str());
-	std::cerr << "apres system\n";	
+	std::system(cmd.c_str());
 	char buf[BUFFER_SIZE_CGI_PIPE + 1] = {0};
-	// while (read(fd[0], buf, BUFFER_SIZE_CGI_PIPE) > 0)
-	read(fd[0], buf, BUFFER_SIZE_CGI_PIPE);
-	
+	while (read(fd[0], buf, BUFFER_SIZE_CGI_PIPE) == BUFFER_SIZE_CGI_PIPE){	
 		displayList += buf;
 		memset(buf, 0, BUFFER_SIZE_CGI_PIPE + 1);
-	
-	// displayList += buf;
+	}
+	displayList += buf;
 
 	close(fd[0]);	
-	dup2(STDOUT_FILENO, coutSave);
-	
-}
-
-int main(void)
-{
-	std::cout << autoIndexDisplayer("./") << "\n";
+	dup2(coutSave, STDOUT_FILENO);
 }
