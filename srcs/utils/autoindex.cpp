46bd6fb1161c7ce1c8a720d3c9f54c60fcb49335
Lucas LEFRANCQ <lucas.lefrancq@hotmail.fr>
2021-06-18 10:29:10 +0200

    move php bin in a bin directory

--- srcs/utils/autoindex.cpp
+++ srcs/utils/autoindex.cpp
@@ -6,7 +6,7 @@
 /*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 17:42:35 by hherin            #+#    #+#             */
-/*   Updated: 2021/06/17 20:12:13 by llefranc         ###   ########.fr       */
+/*   Updated: 2021/06/18 09:56:39 by llefranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void autoIndexDisplayer(std::string const &uri, std::string* displayList)
     if (!(path = getcwd(NULL, 0)))
         throw StatusLine(500, REASON_500, "getcwd function in autoIndexDisplayer failed\n");
 
-	std::string exec_path = "php " + (path + ("/srcs/utils/autoindex.php " + uri)) ;
+	std::string exec_path = "./bin/php " + (path + ("/srcs/utils/autoindex.php " + uri));
 	std::system(exec_path.c_str());
 
     // Restoring STDOUT and closing the last fd[1], so read will get EOF signal
